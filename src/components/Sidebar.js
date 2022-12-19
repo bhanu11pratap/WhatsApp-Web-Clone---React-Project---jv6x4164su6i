70bf90f2a4791e46f3b4bfe284d5b7b9d263ba4e
bhanu11pratap <110561155+bhanu11pratap@users.noreply.github.com>
2022-12-19 19:30:24 +0530

    first commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection , onSnapshot } from "firebase/firestore";
 import { db } from "../firebase";
 import SidebarChat from "./SidebarChat";
 
